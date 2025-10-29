0ad088aa7617347617c7bf2669d1b850e1b99bed
Ubuntu <ubuntu@ip-172-31-9-26.eu-west-1.compute.internal>
2025-10-29 16:08:58 +0000

    set data split

---
+++ src/data/split_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("data/raw_data/minerals_raw_data.csv")
+
+# Définition des variables
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]   # = silica_concentrate
+
+# Division train et test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+X_train.to_csv("data/processed_data/X_train.csv", index=False)
+X_test.to_csv("data/processed_data/X_test.csv", index=False)
+y_train.to_csv("data/processed_data/y_train.csv", index=False)
+y_test.to_csv("data/processed_data/y_test.csv", index=False)
+
+print("Data split completed : data/processed_data/")
